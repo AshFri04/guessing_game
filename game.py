7f2d74f503485525090fba4a94d159e644a0cf52
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 15:50:10 -0700

    Added input range detector

--- game.py
+++ game.py
@@ -7,12 +7,15 @@ def number_guessing_game():
     guess = 0
     count = 0
     while(guess != secret_number):
-        guess = int(raw_input("You guess? "))
-        count += 1
-        if guess > secret_number:
-            print "Your guess is too high, try again."
-        elif guess < secret_number:
-            print "Your guess is too low, try again."
+        guess = int(raw_input("Your guess? "))
+        if guess in range (1, 100):
+            count += 1
+            if guess > secret_number:
+                print "Your guess is too high, try again."
+            elif guess < secret_number:
+                print "Your guess is too low, try again."
+            else:
+                print "Well done, {}! You found my number in {} tries!".format(name, count)
         else:
-            print "Well done, {}! You found my number in {} tries!".format(name, count)
+            print "Oops! That number is not in range 1-100. Please guess again within range."
 number_guessing_game()
